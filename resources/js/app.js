f6addb310560c5b1252644a7d96475312cc149c0
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2023-09-27 10:30:29 +0600

    added vue-router, and divided layout from blade to vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,24 @@
 import { TailwindPagination } from 'laravel-vue-pagination';
+import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue/dist/vue.esm-bundler';
 
 import './bootstrap';
+import PostCreate from './components/Posts/Create.vue';
 import PostsIndex from './components/Posts/Index.vue';
+import App from './layouts/App.vue';
 
-const app = createApp({})
-app.component('posts-index', PostsIndex)
+const routes = [
+    { path: '/', component: PostsIndex },
+    { path: '/posts/create', component: PostCreate },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App)
+app.use(router)
+
 app.component('Pagination', TailwindPagination)
 app.mount('#app')
---
+++ resources/js/components/Posts/Create.vue
@@ -0,0 +1,3 @@
+<template>
+    Create component
+</template>
